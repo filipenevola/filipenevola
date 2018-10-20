49c4663e08c42f21891c1db2fc09693194fd450a
filipenevola <filipemkv@gmail.com>
2018-10-20 11:30:35 -0400

    updating tips page

--- pages/tips.js
+++ pages/tips.js
@@ -5,10 +5,28 @@ import { Content } from "../components/content";
 
 const TEXT = {
   en: {
-    soon: key => <p key={key}>Coming soon ...</p>
+    0: key => (
+      <Fragment key={key}>
+        <a href="https://twitter.com/i/moments/1053667928749600768">
+          For previous tips click here to see Tweets with them
+        </a>
+        <br />
+        <br />
+        For future tips subscribe below
+      </Fragment>
+    )
   },
   pt: {
-    soon: key => <p key={key}>Em breve ...</p>
+    0: key => (
+      <Fragment key={key}>
+        <a href="https://twitter.com/i/moments/1053667928749600768">
+          Para dicas passadas clique aqui para ver os Tweets
+        </a>
+        <br />
+        <br />
+        Para dicas futuras se inscreva abaixo
+      </Fragment>
+    )
   }
 };
 export default () => (
