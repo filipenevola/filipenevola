82bc213dbfc06fb2e4dab18aea4712b11cabf356
Filipe Névola <filipe@quave.com.br>
2022-03-01 08:13:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const TEXT = {
         href="https://www.youtube.com/channel/UCZo_DmCzwmxMtv5Kv_dukpA">"Como
         Criar um App"</a>.<br/><br/>
         I also have a <a
-        href="https://www.youtube.com/channel/UCAfhf9uh1osU2CW0fKrZ8xA">channel</a> about
+        href="https://www.youtube.com/channel/UC4Qdgi2wJxNOQ5JQ3wJBGJw">channel</a> about
         business but only in Portuguese.<br/><br/>
         <a href="https://twitter.com/meteorjs">Meteor</a>'s CEO, Developer
         & Instructor trying to be better every day, founder of{" "}
@@ -32,7 +32,7 @@ const TEXT = {
         href="https://www.youtube.com/c/HowToCreateAnAppDev">"How to Create an
         App"</a>.<br/><br/>
         Vamos conversar sobre Negócios? <a
-        href="https://www.youtube.com/channel/UCAfhf9uh1osU2CW0fKrZ8xA">"Provocações
+        href="https://www.youtube.com/channel/UC4Qdgi2wJxNOQ5JQ3wJBGJw">"Provocações
         sobre Negócios"</a> é o meu canal onde falo sobre Dinheiro e Negócios de
         uma forma diferente.<br/><br/>
         CEO do <a href="https://twitter.com/meteorjs">Meteor</a>,
